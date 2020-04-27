8b47bf4b4f6784f6dbdbe9bf079061be2efe74a9
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-04-27 13:34:19 +0800

    check in upload to sharepoint o365

---
+++ ConAppUploadDocumentSubFloderO365/ConAppUploadDocumentO365/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppUploadDocumentO365
+{
+    class Login
+    {
+        public static SecureString GetPassword()
+        {
+            string password = "passwordHere";
+            SecureString securePassword = new SecureString();
+            
+            foreach(char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+            return securePassword;
+        }
+    }
+}
---
+++ ConAppUploadDocumentSubFloderO365/ConAppUploadDocumentO365/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+using System.Security;
+using System.IO;
+
+namespace ConAppUploadDocumentO365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string siteUrl = "http://SharePoint/MySite";
+            //changed the path below according to what you have
+            string siteUrl = "https://ptp.sharepoint.com/";
+
+            ClientContext clientContext = new ClientContext(siteUrl);
+
+            SecureString securePassword = Login.GetPassword();
+
+            clientContext.Credentials = new SharePointOnlineCredentials("login@ptp.com.my", securePassword);
+
+            Web rootWeb = clientContext.Web;
+
+            //using (FileStream fs = new FileStream(@"sperror.txt", FileMode.Open))
+            //{
+            //    Microsoft.SharePoint.Client.File.SaveBinaryDirect(clientContext, "/Documents/Topfolder/sperror.txt", fs, true);
+            //}
+
+            string filePath = "sperror.txt";
+
+            FileCreationInformation newFile = new FileCreationInformation();
+
+            newFile.Content = System.IO.File.ReadAllBytes(filePath);
+            newFile.Url = System.IO.Path.GetFileName(filePath);
+
+            SP.List oList = clientContext.Web.Lists.GetByTitle(@"Documents");
+
+            //Add Folder 
+            var folders = oList.RootFolder.Folders;
+            clientContext.Load(folders);
+            clientContext.ExecuteQuery();
+
+            var folder = folders.Where(r => r.Name == "TopFolder");
+            var folder1 = folder.FirstOrDefault();
+
+            //Microsoft.SharePoint.Client.File uploadFile = oList.RootFolder.Files.Add(newFile);
+            Microsoft.SharePoint.Client.File uploadFile = folder1.Files.Add(newFile);
+
+            clientContext.Load(uploadFile);
+
+            clientContext.ExecuteQuery();
+
+            SP.ListItem item = uploadFile.ListItemAllFields;
+            string docTitle = string.Empty;
+
+            item["Title"] = docTitle;
+            item.Update();
+
+            clientContext.ExecuteQuery();
+
+        }
+    }
+}
